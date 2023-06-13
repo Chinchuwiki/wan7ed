04d661c308aadf72f2701685e66784c3fd10f29b
GastlyVirus <gstnedgrdrch93@gmail.com>
2023-06-13 17:00:08 -0300

    Se hace primera subida

---
+++ index.js
@@ -0,0 +1,101 @@
+function cambiarModal(id) {
+  let titulo = "";
+  let texto = "";
+  let apple = "";
+  let spotify = "";
+
+  if (id === "album1") {
+    titulo = "Whatever People Say I Am, That's What I'm Not";
+    texto =
+      "es el álbum debut de la banda de rock inglesa Arctic Monkeys, lanzado el 23 de enero de 2006 bajo el sello Domino Records y grabado en Chapel Studios, Lincolnshire y 2 Fly Studios, Sheffield. El disco se convertiría en el álbum debut de venta más rápida en toda la historia del Reino Unido, superando así el álbum homónimo del grupo Elastica, vendiendo más de 360.000 copias en su primera semana de lanzamiento, y desde entonces manteniéndose como el álbum debut de venta más rápida por una banda. El álbum es séxtuple platino en el Reino Unido.";
+
+    ("El álbum incluye las dos canciones (Fake Tales of San Francisco y From the Ritz to the Rubble) que componían el EP debut de la banda, Five Minutes with Arctic Monkeys, así como los dos primeros sencillos de la banda los cuales quedaron en #1 en el Reino Unido, I Bet You Look Good on the Dancefloor y When the Sun Goes Down.");
+
+    ("Ganaría el Mercury Prize al mejor álbum en 2006, y estaría en la posición #371 en la lista de los 500 mejores álbumes de todos los tiempos según Rolling Stone. En 2013, Rolling Stone lo posicionaría en el #30 de su lista «100 Best Debut Albums of All Time».");
+    apple =
+      "https://music.apple.com/es/album/whatever-people-say-i-am-thats-what-im-not/111153953";
+    spotify =
+      "https://open.spotify.com/album/0ndGMh4twJNzPpr5XtHTR2?si=Y0CRpUz3RWaxT5UACbhooQ";
+  } else if (id === "album2") {
+    titulo = "Favourite Worst Nightmare";
+    texto =
+      "es el segundo álbum de estudio de Arctic Monkeys. Fue lanzado por primera vez en Japón el 18 de abril de 2007 y grabado en Miloco Studios con productores como James Ford y Mike Crossey. En su primera semana, el álbum vendió más de 200.000 copias, emulando lo sucedido con Whatever People Say I Am, That's What I'm Not. “Brianstorm”, lanzado el 16 de abril de 2007, es el primer sencillo del álbum. Alex Turner describió las nuevas canciones como 'muy diferentes de la última vez', agregando que el sonido de algunos tracks son 'un poco como 'From the Ritz to the Rubble', 'The View from the Afternoon', ese tipo de cosas.' El 29 de abril de 2007, “Favourite Worst Nightmare” llegó al puesto número #1 en UK Albums Chart. Además, los 12 temas del álbum quedaron en el Top 200 de UK Singles Chart, llegando “Brianstorm” al puesto número #2 y “If You Were There, Beware” al puesto número #189. El 27 de abril de 2007 tienen un total de 18 temas en el Top 200. «Fluorescent Adolescent» y '505' entran en el Top 75, alcanzando los puestos número #60 y #74 respectivamente.";
+    apple =
+      "https://music.apple.com/us/album/favourite-worst-nightmare/251499221";
+    spotify =
+      "https://open.spotify.com/album/1XkGORuUX2QGOEIL4EbJKm?si=nZjbGcMBRwePHq9AS_BzLg";
+  } else if (id === "album3") {
+    titulo = "Humbug";
+    texto =
+      "es el tercer álbum de estudio de la banda inglesa de indie rock Arctic Monkeys lanzado el 19 de agosto de 2009 por Domino Records. La banda comenzó a escribir canciones para el álbum a partir del verano de 2008, y lo terminó completamente en primavera del 2009. Como su álbum pasado, Favourite Worst Nightmare (2007), Humbug fue lanzado primeramente en Japón el 19 de agosto de 2009, seguido por Australia, Brasil, Irlanda y Alemania el 21 de agosto de 2009. Fue luego lanzado en el Reino Unido el 24 de agosto de 2009, en los Estados Unidos un día después y en Grecia el 31 de agosto.";
+    apple = "https://music.apple.com/us/album/humbug/335649043?l=es";
+    spotify =
+      "https://open.spotify.com/album/5ZnOKznPxZTWuMusR4tmGG?si=_djATQ4NQRKxsDoJodimPQ";
+  } else if (id === "album4") {
+    titulo = "Suck It and See";
+    texto =
+      "es el cuarto álbum de estudio de la banda inglesa Arctic Monkeys. Fue lanzado oficialmente el 6 de junio de 2011 aunque para el 17 de mayo de 2011 el disco se había filtrado y ya se podía descargar de varios lugares.";
+    ("Suck It and See ha recibido críticas positivas de los críticos, con una calificación promedio de 74% en Metacritic , y ha cumplido con un éxito notable en público, el álbum fue directo al número 1 en su primera semana en el Reino Unido, además de ser generalmente alabado como un 'retorno a la forma', después del 'más difícil' tercer álbum de la banda, Humbug. NME ha argumentado sobre el arte de tapa del álbum, dándole el nombre de uno de los peores 50 en la historia. Además la portada del álbum fue censurada, poniendo de fondo solo las palabras 'Suck It and See'. En julio, el álbum ganó el premio Mojo para el Mejor Álbum de 2011. También recibió una crítica muy positiva por parte de coda musical. El álbum vendió más de 82 000 unidades en su primera semana en el Reino Unido y fue directo al número uno. En su segunda semana, vendió más 34 910 unidades en el Reino Unido. En general, el álbum vendió 154 000 unidades en su primera semana en todo el mundo.");
+    apple = "https://music.apple.com/us/album/suck-it-and-see/434497770?l=es";
+    spotify =
+      "https://open.spotify.com/album/2ym2jcqckXqWeTDoxz3Kst?si=GSTs4ozoR9CGWm8N8YWd1w";
+  } else if (id === "album5") {
+    titulo = "AM";
+    texto =
+      "es el quinto álbum de estudio de la banda inglesa de Indie Rock, Arctic Monkeys. Fue lanzado el 9 de septiembre de 2013, bajo el sello Domino Records. El 15 de julio de 2013, revelaron la portada del álbum. El disco cuenta con las colaboraciones de Josh Homme, Bill Ryder-Jones y Pete Thomas. Fue producido por James Ford y coproducido por Ross Orton en los estudios Sage & Sound Recording en Los Ángeles y Rancho de la Luna en Joshua Tree, California.";
+    ("El álbum recibió elogios de la crítica. Fue nominado para el 2013 Mercury Prize al mejor álbum, y aparece en el número 449 en la lista de los 500 mejores álbumes de todos los tiempos, según la revista NME. Comercialmente, AM se ha convertido en el álbum más exitoso de Arctic Monkeys hasta la fecha, encabezó las listas en varios países, y llegó a las diez primeras posiciones en muchos más. En el Reino Unido, Arctic Monkeys rompió un récord con AM, convirtiéndose en la primera banda indie en debutar en el número uno en el Reino Unido con sus cinco primeros álbumes.");
+    apple = "https://music.apple.com/us/album/am/663097964?l=es";
+    spotify =
+      "https://open.spotify.com/album/78bpIziExqiI9qztvNFlQu?si=cbBECWMaQ5GQJf8acu91DA";
+  } else if (id === "album6") {
+    titulo = "Tranquility Base Hotel & Casino";
+    texto =
+      "es el sexto álbum de estudio de la banda de rock inglesa Arctic Monkeys, lanzado el 11 de mayo de 2018 por Domino Records. El disco sería completamente compuesto en 2016 por el vocalista de la banda, Alex Turner, en un piano Steinway Vertegrand en su casa de Los Ángeles, siendo producido un año más tarde por él mismo y James Ford, entre las ciudades de Los Ángeles, París y Londres. El álbum además cuenta con colaboración de múltiples artistas invitados, incluyendo a Tom Rowley, Loren Humphrey, James Righton, Zach Dawes, Tyler Parkford y Cam Avery. Turner también diseñó la portada, que representa el complejo turístico con recortes de cartón y un magnetófono; el nombre del álbum hace referencia a la Base Tranquilidad, lugar del alunizaje del Apolo 11.";
+    apple =
+      "https://music.apple.com/us/album/tranquility-base-hotel-casino/1367926252?l=es";
+    spotify =
+      "https://open.spotify.com/album/1jeMiSeSnNS0Oys375qegp?si=sph-tpGETBK1L39ifF5e7A";
+  } else if (id === "album7") {
+    titulo = "The Car";
+    texto =
+      "es el séptimo álbum de estudio de la banda de rock inglesa Arctic Monkeys, publicado el 21 de octubre de 2022 por Domino Records. El disco está compuesto por el vocalista de la banda, Alex Turner, con producción de James Ford, y fue grabado en las ciudades de París y Londres en el verano de 2021.";
+    apple = "https://music.apple.com/us/album/the-car/1639369016?l=es";
+    spotify =
+      "https://open.spotify.com/album/2GROf0WKoP5Er2M9RXVNNs?si=r0QdOLLiTNWMiEpbNIzbQQ";
+  }
+
+  let modalTitulo = document.getElementById("modalTitulo");
+  let modalTexto = document.querySelector(".modalTexto");
+  let modalApple = document.getElementById("apple");
+  let modalSpotify = document.getElementById("spotify");
+
+  modalTitulo.textContent = titulo;
+  modalTexto.textContent = texto;
+  modalApple.href = apple;
+  modalSpotify.href = spotify;
+}
+
+function cambioMiembroFoto(id) {
+  let miembro;
+  let foto;
+
+  if (id === "foto1") {
+    miembro = "Alex Tuner";
+    foto = "./Imagenes/miembro-alex.jpg";
+  } else if (id === "foto2") {
+    miembro = "Matt Helders";
+    foto = "./Imagenes/miembro-matt.jpg";
+  } else if (id === "foto3") {
+    miembro = "Nick O'Malley";
+    foto = "./Imagenes/miembro-nick.jpg";
+  } else if (id === "foto4") {
+    miembro = "Jamie Cook";
+    foto = "./Imagenes/miembro-jamie.jpg";
+  }
+
+  let tituloMiembro = document.getElementById("tituloMiembro");
+  let fotoMiembro = document.getElementById("fotoMiembro");
+
+  tituloMiembro.textContent = miembro;
+  fotoMiembro.src = foto;
+}
